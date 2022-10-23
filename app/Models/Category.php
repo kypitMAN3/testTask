4b134c3d520ed35a1fc1bb29a39bcdf8a3b9ce56
Mark Gerbersgagen <kypitman2@gmail.com>
2022-10-23 13:45:01 +0300

    Create Category.php
    feat: created categories model and added reations

---
+++ app/Models/Category.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Category extends Model
+{
+    use HasFactory;
+
+    public function childs(): HasMany
+    {
+        return $this->hasMany(self::class, 'parent_id', 'id');
+    }
+
+    public function parents(): HasOne
+    {
+        return $this->hasOne(self::class);
+    }
+}
