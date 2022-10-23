93ea49e58e971aef6154d91781b431b9cf4ab689
Mark Gerbersgagen <kypitman2@gmail.com>
2022-10-23 20:08:53 +0300

    Update web.php
    fix: added routing for tree and flat lists

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [CategoryController::class, 'index']);
+Route::get('/show', [CategoryController::class, 'show']);
