abcf01fd1fe412143166605e06f9fcf803a2e7d1
Mark Gerbersgagen <kypitman2@gmail.com>
2022-10-23 20:07:43 +0300

    Update 2022_10_23_103127_create_categories_table.php
    fix: changed Category migration

--- database/migrations/2022_10_23_103127_create_categories_table.php
+++ database/migrations/2022_10_23_103127_create_categories_table.php
@@ -14,9 +14,9 @@ class CreateCategoriesTable extends Migration
     public function up()
     {
         Schema::create('categories', function (Blueprint $table) {
-            $table->id();
+            $table->bigIncrements('id');
             $table->string('name');
-            $table->integer('parent_id')->nullable();
+            $table->unsignedInteger('parent_id')->nullable();
             $table->timestamps();
         });
     }
