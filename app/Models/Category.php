ba4100e55c87854a08495e7a614bed6d45e614cd
Mark Gerbersgagen <kypitman2@gmail.com>
2022-10-23 20:08:18 +0300

    Update Category.php
    fix: changed relation

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -5,18 +5,17 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Category extends Model
 {
     use HasFactory;
 
-    public function childs(): HasMany
-    {
-        return $this->hasMany(self::class, 'parent_id', 'id');
+    public function childs(){
+        return $this->hasMany($this, 'parent_id');
     }
 
-    public function parents(): HasOne
-    {
-        return $this->hasOne(self::class);
+    public function rootCategories(){
+        return $this->where('parent_id', null)->with('childs')->get();
     }
 }
